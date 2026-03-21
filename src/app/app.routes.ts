adbdc484ccb7c7f7e2a7bea7da64c972920f0c14
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-21 16:46:37 -0300

    Creating router system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ShardSquadPage } from './pages/shard-squad-page/shard-squad-page.component';
+import { MainContent } from './main-content/main-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainContent
+    },
+    {
+        path: 'shard-squad',
+        component: ShardSquadPage
+    }
+];
---
+++ src/app/pages/shard-squad-page/shard-squad-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shard-squad-page',
+  imports: [],
+  templateUrl: './shard-squad-page.component.html',
+  styleUrl: './shard-squad-page.component.scss',
+})
+export class ShardSquadPage {}
