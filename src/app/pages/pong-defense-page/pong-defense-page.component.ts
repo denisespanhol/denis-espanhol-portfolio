e37f3ef9be44af5c73e81cd96a54c254fd212024
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-25 11:09:55 -0300

    Creating pong-defense-page component

---
+++ src/app/pages/pong-defense-page/pong-defense-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pong-defense-page',
+  imports: [],
+  templateUrl: './pong-defense-page.component.html',
+  styleUrl: './pong-defense-page.component.scss',
+})
+export class PongDefensePage {}
